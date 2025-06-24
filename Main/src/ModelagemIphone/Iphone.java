58575f1a4dbffd1c5ce490ec1a411ce57ca1c4fa
Ricardo Silva <kisilva2010@hotmail.com>
2025-06-24 00:37:36 -0300

    Adicionando nova Atividade de Modelagem de Iphone Proposta pelo Curso da DIO

---
+++ Main/src/ModelagemIphone/Iphone.java
@@ -0,0 +1,25 @@
+package ModelagemIphone;
+
+// Modelagem e Diagramação de um Componente iPhone
+
+// Neste desafio, você será responsável por modelar e diagramar a representação UML do componente iPhone, 
+// abrangendo suas funcionalidades como Reprodutor Musical, Aparelho Telefônico e Navegador na Internet.
+
+// Funcionalidades a Modelar
+// Reprodutor Musical Métodos: tocar(), pausar(), selecionarMusica(String musica)
+// Aparelho Telefônico Métodos: ligar(String numero), atender(), iniciarCorreioVoz()
+// Navegador na Internet Métodos: exibirPagina(String url), adicionarNovaAba(), atualizarPagina()
+
+// Objetivo
+// Criar um diagrama UML que represente as funcionalidades descritas acima.
+// Implementar as classes e interfaces correspondentes em Java (Opcional).
+
+public class Iphone {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+    
+}
---
+++ Main/src/ModelagemIphone/NavegadorIntenet.java
@@ -0,0 +1,5 @@
+package ModelagemIphone;
+
+public class NavegadorIntenet {
+    
+}
